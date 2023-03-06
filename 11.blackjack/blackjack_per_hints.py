21e549f265a5a4e6fa15582dd926b64180d51d6b
aleksandr_markhel <volatpy@gmail.com>
2023-03-06 23:23:57 +0300

    per hints start

---
+++ 11.blackjack/blackjack_per_hints.py
@@ -0,0 +1,6 @@
+import random
+#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
+#11 is the Ace.
+#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def deal_card(deck):
+    return
