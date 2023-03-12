83c33e2b8a9a02323e3232e89572150302748441
aleksandr_markhel <volatpy@gmail.com>
2023-03-12 21:52:10 +0300

    ard add

--- 15.coffee_mashine/coffe_machine.py
+++ 15.coffee_mashine/coffe_machine.py
@@ -1,3 +1,5 @@
 # TODO: 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):
 
 # TODO: 2. Turn off the Coffee Machine by entering “off” to the prompt.
+
+# TODO: 3. Print report.
