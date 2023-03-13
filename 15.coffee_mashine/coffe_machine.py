e2434ed0b06fe757337fe251ed2f49f535e35c8d
aleksandr_markhel <volatpy@gmail.com>
2023-03-13 21:35:31 +0300

    15. one step plus

--- 15.coffee_mashine/coffe_machine.py
+++ 15.coffee_mashine/coffe_machine.py
@@ -1,15 +1,23 @@
 from art import art_text, emoji
 
-# TODO: 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):
-user_choice = input("What would you like? (espresso (cost: 1.5)/latte (cost: 2.5)"
-                    "/cappuccino (cost: 3.0))").lower()
-print("Please insert coins.")
-print("How many quarters?")
-print("How many dims?")
-print("How many nickles?")
-print("How many pennies?")
-print("Sorry, that's not enough money. Money refunded.")
-print(f"Hire is {} in change.")
-print(f"Hire is your {} {emoji}. Enjoy!")
+
+# def user_choice(user_choice):
+#     if user_choice == "espresso" or user_choice == "latte" or user_choice == "cappuccino":
+#         pass
+#     if user_choice == "report":
+#         pass
+
+# print(art_text)
+# user_choice = input("What would you like? (espresso (cost: 1.5)/latte (cost: 2.5)"
+#                     "/cappuccino (cost: 3.0))").lower()
+# print("Please insert coins.")
+# print("How many quarters?")
+# print("How many dims?")
+# print("How many nickles?")
+# print("How many pennies?")
+# print("Sorry, that's not enough money. Money refunded.")
+# print(f"Hire is {} in change.")
+# print(f"Hire is your {} {emoji}. Enjoy!")
 
 
+def make_coffe()
