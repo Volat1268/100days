4731efa599ffe270bb2c845a1917a51a7dc5e7a4
Volatby <alemarkhel@yandex.ru>
2023-03-14 17:09:18 +0300

    15. first functions and variables

--- 15.coffee_mashine/coffe_machine.py
+++ 15.coffee_mashine/coffe_machine.py
@@ -1,11 +1,50 @@
 from art import art_text, emoji
+from data import MENU, resources
 
 
-# def user_choice(user_choice):
-#     if user_choice == "espresso" or user_choice == "latte" or user_choice == "cappuccino":
-#         pass
-#     if user_choice == "report":
-#         pass
+# ------------------------functions------------------------------------------
+
+
+def user_choice(user_choice):
+    if user_choice == "espresso" or user_choice == "latte" or user_choice == "cappuccino":
+        return make_coffee()
+    if user_choice == "report":
+        return report()
+    if user_choice == "of":
+        return take_off()
+    else:
+        pass
+
+
+def make_coffee():
+    pass
+
+
+def report():
+    pass
+
+
+def take_off():
+    pass
+
+
+# ----------------------- variables-----------------------------------------------
+water_left = resources["water"]
+milk_lef = resources["milk"]
+coffee_lef = resources["coffee"]
+espresso_cost = MENU["espresso"]["cost"]
+latte_cost = MENU["latte"]["cost"]
+cappuccino_cost = MENU["cappuccino"]["cost"]
+espresso_water_use = MENU["espresso"]["ingredients"]["water"]
+espresso_coffee_use = MENU["espresso"]["ingredients"]["coffee"]
+latte_water_use = MENU["latte"]["ingredients"]["water"]
+latte_coffee_use = MENU["latte"]["ingredients"]["coffee"]
+cappuccino_water_use = MENU["cappuccino"]["ingredients"]["water"]
+cappuchino_coffee_use = MENU["cappuccino"]["ingredients"]["coffee"]
+
+# ----------------------- programme-------------------------------------
+
+
 
 # print(art_text)
 # user_choice = input("What would you like? (espresso (cost: 1.5)/latte (cost: 2.5)"
@@ -20,4 +59,4 @@ from art import art_text, emoji
 # print(f"Hire is your {} {emoji}. Enjoy!")
 
 
-def make_coffe()
+mat_choice(2)
